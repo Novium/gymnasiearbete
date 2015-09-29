d8be5866f3a2d54419929a4092eb00630a998040
novium_ <mail@novium.pw>
2015-09-29 13:37:38 +0200

    missed a not-equal, code returned unintended value 0

--- compute.js
+++ compute.js
@@ -7,23 +7,27 @@
 var filter = {
 median:
   function (data, window) {
-    // Only works for odd sized windows above 1
-    if(window % 2 == 1)
+    // Only works for odd sized windows above 1 due to the median function
+    if(window % 2 != 1)
       return 0;
 
+    // Calculate median value for a given array
     function median(arr) {
       var arr, i;
+      // i = odd array (eg. 5) divided with 2 floored
       i = Math.floor(arr.length / 2);
       arr = arr.sort()[i];
-
       return arr;
     }
 
+    // DEV CODE - RETURN MEDIAN FUNCTION
     return median(data);
   }
 };
 
+// DEV CODE - TEST DATA
 var testData1 = [2, 3, 2, 4, 4, 10, 3, 2];
-var testData2 = [2, 5, 3];
+var testData2 = [2, 10, 3];
+var testData3 = [3, 5, 20, 5, 5, 2, 4]
 
 console.log(filter.median(testData2, 3));
